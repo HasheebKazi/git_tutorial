c6325fb894dcf330966b39717c8404a9d85c64d9
HasheebKazi <hasheeb.k@gmail.com>
2020-02-29 19:46:30 -0500

    more stuff

--- main.py
+++
@@ -1,9 +0,0 @@
-lst = [2.0, 0.0, 5.0]
-
-for e in lst:
-    if not e == 0 or e == 0.0:
-        print('The reciprocal of {} is {}'.format(e , 1 / e));
-    else:
-        print('The {} has no reciprocal.'.format(e));
-
-x = 2; # a useless variable
